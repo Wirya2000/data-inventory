679a27868ff61570bc49313650f76d972141a756
wirya <wirya.wonggo@sby.spil>
2025-10-20 16:37:52 +0700

    add

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -30,4 +30,132 @@ class ReportController extends Controller
 
         return view('pages.admin.laporan.penjualanDetail', compact('laporan', 'startDate', 'endDate'));
     }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function penjualanRekap(Request $request)
+    {
+        $startDate = $request->start_date;
+        $endDate = $request->end_date;
+        // if ($startDate != null && $endDate != null) {
+            $laporan = Penjualan::with(['customer', 'barangs'])
+            ->whereBetween('tanggal', [$startDate, $endDate])
+            ->select('id', 'tanggal', 'customer_id', 'discount', 'grand_total')
+            ->get()
+            ->map(function ($penjualan) {
+                // hitung total item & subtotal per transaksi
+                $totalItem = $penjualan->barangs->sum('pivot.jumlah');
+                $subtotal = $penjualan->barangs->sum(function ($barang) {
+                    return $barang->pivot->jumlah * $barang->pivot->harga_satuan;
+                });
+
+                return [
+                    'tanggal'      => $penjualan->tanggal,
+                    'no_transaksi' => $penjualan->id,
+                    'customer'     => $penjualan->customer->nama ?? $penjualan->nama_customer,
+                    'total_item'   => $totalItem,
+                    'subtotal'     => $subtotal,
+                    'discount'     => $penjualan->discount,
+                    'grand_total'  => $penjualan->grand_total,
+                ];
+            });
+        // }
+
+        return view('pages.admin.laporan.penjualanRekap', compact('laporan', 'startDate', 'endDate'));
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function penjualanHarian(Request $request)
+    {
+        $startDate = $request->start_date;
+        $endDate = $request->end_date;
+        // if ($startDate != null && $endDate != null) {
+            $laporan = Penjualan::with([
+                    'barangs',
+                    'customer:id,nama',
+                    'user:id,nama'
+                ])
+                ->whereBetween('tanggal', [$startDate, $endDate])
+                ->orderBy('tanggal', 'asc')
+                ->get();
+        // }
+
+        return view('pages.admin.laporan.penjualanHarian', compact('laporan', 'startDate', 'endDate'));
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function penjualanPerBarang(Request $request)
+    {
+        $startDate = $request->start_date;
+        $endDate = $request->end_date;
+        // if ($startDate != null && $endDate != null) {
+            $laporan = Penjualan::with([
+                    'barangs',
+                    'customer:id,nama',
+                    'user:id,nama'
+                ])
+                ->whereBetween('tanggal', [$startDate, $endDate])
+                ->orderBy('tanggal', 'asc')
+                ->get();
+        // }
+
+        return view('pages.admin.laporan.penjualanPerBarang', compact('laporan', 'startDate', 'endDate'));
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function penjualanPerCustomer(Request $request)
+    {
+        $startDate = $request->start_date;
+        $endDate = $request->end_date;
+        // if ($startDate != null && $endDate != null) {
+            $laporan = Penjualan::with([
+                    'barangs',
+                    'customer:id,nama',
+                    'user:id,nama'
+                ])
+                ->whereBetween('tanggal', [$startDate, $endDate])
+                ->orderBy('tanggal', 'asc')
+                ->get();
+        // }
+
+        return view('pages.admin.laporan.penjualanPerCustomer', compact('laporan', 'startDate', 'endDate'));
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function penjualanProfitPenjualan(Request $request)
+    {
+        $startDate = $request->start_date;
+        $endDate = $request->end_date;
+        // if ($startDate != null && $endDate != null) {
+            $laporan = Penjualan::with([
+                    'barangs',
+                    'customer:id,nama',
+                    'user:id,nama'
+                ])
+                ->whereBetween('tanggal', [$startDate, $endDate])
+                ->orderBy('tanggal', 'asc')
+                ->get();
+        // }
+
+        return view('pages.admin.laporan.penjualanProfitPenjualan', compact('laporan', 'startDate', 'endDate'));
+    }
 }
---
+++ resources/views/pages/admin/laporan/penjualanRekap.blade.php
@@ -0,0 +1,101 @@
+@extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
+
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Tables'])
+    <div class="container-fluid py-4">
+        <div class="row">
+            <div class="col-12">
+                <div class="card mb-4">
+                    <div class="card-header pb-0">
+                        <h6>Laporan Penjualan Rekap</h6>
+                        {{-- <a href="{{ route('kategoris.create') }}" class="btn btn-primary">Add Kategori</a> --}}
+                    </div>
+                    <div class="card-body px-0 pt-0 pb-2">
+                        <div class="row px-4">
+                            <!-- Filter Form -->
+                            <form method="GET" action="{{ route('reports.penjualanRekap') }}" class="mb-3">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label for="start_date">Tanggal Mulai</label>
+                                            <input type="date" id="start_date" name="start_date"
+                                                class="form-control datepicker"
+                                                value="{{ $startDate }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label for="end_date">Tanggal Selesai</label>
+                                            <input type="date" id="end_date" name="end_date"
+                                                class="form-control datepicker"
+                                                value="{{ $endDate }}">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3 d-flex align-items-end">
+                                        <button type="submit" class="btn btn-primary">Filter</button>
+                                    </div>
+                                </div>
+                            </form>
+                            <p>Periode: {{ $startDate }} s/d {{ $endDate }}</p>
+                            <div class="table-responsive p-0">
+                                <table id="table-display" class="table table-striped table-hover table-bordered align-items-center mb-0">
+                                    <thead style="background-color: #fb6340; color: #FFFFFF" class="text-center">
+                                        <tr>
+                                            <th class="text-uppercase text-xxs font-weight-bolder">Tanggal</th>
+                                            <th class="text-uppercase text-xxs font-weight-bolder">No Transaksi</th>
+                                            <th class="text-uppercase text-xxs font-weight-bolder">Customer</th>
+                                            <th class="text-uppercase text-xxs font-weight-bolder">Total Item</th>
+                                            <th class="text-uppercase text-xxs font-weight-bolder">Subtotal</th>
+                                            <th class="text-uppercase text-xxs font-weight-bolder">Discount</th>
+                                            <th class="text-uppercase text-xxs font-weight-bolder">Grand Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($laporan as $penjualan)
+                                            <tr>
+                                                <td>{{ $penjualan->tanggal }}</td>
+                                                <td>{{ $penjualan->id }}</td>
+                                                <td>{{ $penjualan->customer->nama ?? $penjualan->nama_customer }}</td>
+                                                <td>{{ $barang->total_item }}</td>
+                                                <td>{{ number_format($penjualan->subtotal, 0, ',', '.') }}</td>
+                                                <td>{{ number_format($penjualan->discount, 0, ',', '.') }}</td>
+                                                <td>{{ number_format($penjualan->grand_total, 0, ',', '.') }}</td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <tr style="font-weight: bold; background-color: #f6f9fc">
+                                            <td colspan="4" class="text-end">TOTAL</td>
+                                            <td>{{ number_format($laporan->sum('subtotal'), 0, ',', '.') }}</td>
+                                            <td>{{ number_format($laporan->sum('discount'), 0, ',', '.') }}</td>
+                                            <td>{{ number_format($laporan->sum('grand_total'), 0, ',', '.') }}</td>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @include('layouts.footers.auth.footer')
+    </div>
+@endsection
+
+@push('js')
+    <script>
+        $(function() {
+            $(".datepicker").datepicker({
+                dateFormat: "yy-mm-dd"
+            });
+
+            new DataTable('#table-display', {
+                layout: {
+                    topStart: {
+                        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+                    }
+                }
+            });
+        });
+    </script>
+@endpush
